ed3f036ce59ec8890a39b8be4df4885a805cdaf1
Trevor Hendricks <trevorhe@amazon.com>
2020-11-13 20:13:12 +0000

    Resolve issue #6. Improve dynamic endpoint initialization for EFA and SocketLibfabric endpoints. Also improves windows socket initialization

--- src/cdi/internal_rx.c
+++ src/cdi/internal_rx.c
@@ -830,7 +830,8 @@ CdiReturnStatus RxCreateInternal(ConnectionProtocolType protocol_type, CdiRxConf
         }
     }
 
-    if (kCdiStatusOk == rs) {
+    // Socket adapter does not dynamically create Rx endpoints, so create it here.
+    if (kCdiStatusOk == rs && kCdiAdapterTypeSocket == config_data_ptr->adapter_handle->adapter_data.adapter_type) {
         rs = EndpointManagerRxCreateEndpoint(con_state_ptr->endpoint_manager_handle, config_data_ptr->dest_port, NULL);
     }
 
